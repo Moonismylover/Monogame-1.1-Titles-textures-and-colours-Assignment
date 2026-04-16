e79e9201da54df653ffe1fef7cc4cc70a85dda1b
Moonismylover <Angelpreetthind9@gmail.com>
2026-04-16 14:59:14 -0400

    Add project files.

---
+++ Monogame 1.1 - Titles, textures, and colours (Assignment)/Game1.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Monogame_1._1___Titles__textures__and_colours__Assignment_
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        rectangle window;
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            window = new rectangle(0, 0, 800, 500);
+            PreferredBackBufferWidth = 800;
+            PreferredBackBufferHeight = 500;
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            this.window.
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Monogame 1.1 - Titles, textures, and colours (Assignment)/Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new Monogame_1._1___Titles__textures__and_colours__Assignment_.Game1();
+game.Run();
