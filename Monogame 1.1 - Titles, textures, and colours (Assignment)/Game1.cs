52853354467e7b351bbe51d304446f4bb1931c1e
700014473 <700014473@2260I-L29-008>
2026-04-23 12:29:42 -0400

    12345690

--- Monogame 1.1 - Titles, textures, and colours (Assignment)/Game1.cs
+++ Monogame 1.1 - Titles, textures, and colours (Assignment)/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace Monogame_1._1___Titles__textures__and_colours__Assignment_
 {
@@ -16,6 +17,8 @@ namespace Monogame_1._1___Titles__textures__and_colours__Assignment_
         Texture2D aurabgtexture;
         Texture2D glowtexture;
         Texture2D hearttexture;
+
+        int randomX, randomY;
 
         Rectangle window;
         public Game1()
@@ -33,6 +36,11 @@ namespace Monogame_1._1___Titles__textures__and_colours__Assignment_
             _graphics.ApplyChanges();   
 
             this.Window.Title = "Monogame 1.1 - Titles, Textures, and Colours (Assignment)";
+
+            Random generator = new Random();
+
+            randomX = generator.Next(0, window.Width);
+            randomY = generator.Next(0, window.Height);
 
             base.Initialize();
         }
@@ -64,17 +72,34 @@ namespace Monogame_1._1___Titles__textures__and_colours__Assignment_
 
             _spriteBatch.Begin();
             _spriteBatch.Draw(aurabgtexture, new Vector2(0, 0), Color.White);
-            _spriteBatch.Draw(glowtexture, new Vector2(0, 0), Color.White);
             _spriteBatch.Draw(bordertexture, new Vector2(0, 0), Color.White);
-            _spriteBatch.Draw(dovetexture, new Vector2(0, 20), Color.White);
             _spriteBatch.Draw(cloudtexture, new Vector2(10, 0), Color.White);
-
-            for (int i = 0, i <)
-
+            _spriteBatch.Draw(glowtexture, new Vector2(0, 0), Color.White);
             _spriteBatch.Draw(skeletonhandtexture, new Vector2(10, 0), Color.White);
+
+            for (int row = 200; row < 300; row += 50)
+            {
+                for (int col = 50; col < 200; col += 50)
+                {
+                    _spriteBatch.Draw(hearttexture, new Vector2(col, row), Color.White);
+                }
+            }
+
+            _spriteBatch.Draw(dovetexture, new Vector2(randomX, randomY), Color.White);
             _spriteBatch.End();
 
             base.Draw(gameTime);
         }
     }
 }
+
+
+
+
+
+// Try to guarantee that no part of the texture is drawn outside the window.
+//o Load more than one potential background images into your program and randomly
+//choose which one you will set as your background texture.
+// You could randomly choose one in your LoadContent() method.
+//o Generating a random number in a Monogame program is no different than in a console
+//program.
